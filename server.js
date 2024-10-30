b1de78350455cfa88f4e3f8728ca819d2e4d1e51
Lukasz Nowicki <lukasz.nowicki@euvic.pl>
2024-10-30 13:15:51 +0100

    timestamp and page url

--- server.js
+++ server.js
@@ -1,32 +1,31 @@
 const express = require('express');
 const http = require('http');
 const path = require('path');
-const bodyParser = require('body-parser'); // Dodaj body-parser do parsowania JSON
+const bodyParser = require('body-parser');
 
 const app = express();
 const server = http.createServer(app);
 
-// Middleware do parsowania JSON
+
 app.use(bodyParser.json());
 
-// Serwowanie pliku index.html
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-// Serwowanie statycznych plików (np. modeli)
+
 app.use('/models', express.static(path.join(__dirname, 'models')));
 
-// Endpoint do odbierania liczby wykrytych twarzy
 app.post('/faceNumber', (req, res) => {
-  const { faceNumber } = req.body;
+  const { faceNumber,pageUrl,timestamp } = req.body;
   console.log(`Odebrano liczbę twarzy: ${faceNumber}`);
+  console.log(`Odebrano strone: ${pageUrl}`);
+  console.log(`Odebrano timestamp: ${timestamp}`);
   
-  // Odpowiedź do klienta
+  
   res.status(200).send('Liczba twarzy odebrana pomyślnie');
 });
 
-// Start serwera
 const PORT = process.env.PORT || 5000;
 server.listen(PORT, () => {
   console.log(`Serwer działa na porcie ${PORT}`);
