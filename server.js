9b5c9cf761cc921b167d634e43498deb1097ef0c
Lukasz Nowicki <lukasz.nowicki@euvic.pl>
2024-11-05 13:48:29 +0100

    save to file

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const http = require('http');
 const path = require('path');
+const fs = require('fs');
 const bodyParser = require('body-parser');
 
 const app = express();
 const server = http.createServer(app);
-
 
 app.use(bodyParser.json());
 
@@ -13,17 +13,34 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-
 app.use('/models', express.static(path.join(__dirname, 'models')));
 
-app.post('/faceNumber', (req, res) => {
-  const { faceNumber,pageUrl,timestamp } = req.body;
-  console.log(`Odebrano liczbę twarzy: ${faceNumber}`);
-  console.log(`Odebrano strone: ${pageUrl}`);
-  console.log(`Odebrano timestamp: ${timestamp}`);
-  
-  
-  res.status(200).send('Liczba twarzy odebrana pomyślnie');
+app.post('/saveFaceData', (req, res) => {
+  const data = req.body;
+  const filePath = path.join(__dirname, 'faceData.json');
+  fs.appendFile(filePath, JSON.stringify(data) + '\n', (err) => {
+    if (err) {
+      console.error('Błąd zapisu do pliku faceData.json:', err);
+      res.status(500).send('Błąd zapisu do pliku');
+    } else {
+      console.log('Dane wykrycia twarzy zapisane do faceData.json');
+      res.status(200).send('Dane wykrycia twarzy zapisane pomyślnie');
+    }
+  });
+});
+
+app.post('/saveFormData', (req, res) => {
+  const data = req.body;
+  const filePath = path.join(__dirname, 'formData.json');
+  fs.appendFile(filePath, JSON.stringify(data) + '\n', (err) => {
+    if (err) {
+      console.error('Błąd zapisu do pliku formData.json:', err);
+      res.status(500).send('Błąd zapisu do pliku');
+    } else {
+      console.log('Dane formularza zapisane do formData.json');
+      res.status(200).send('Dane formularza zapisane pomyślnie');
+    }
+  });
 });
 
 const PORT = process.env.PORT || 5000;
